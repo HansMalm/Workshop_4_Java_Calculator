ba72bacb547f78fb9808ee729aec903f963871ca
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-02-26 10:42:38 +0100

    modify main.java file
    add simple menu idea

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 
 public class Main {
     static void main() {
+        System.out.println("Welcome to the Calculator!");
+        System.out.println("input 1 for addition");
+        System.out.println("input 2 for subtraction");
+        System.out.println("input 3 for multiplication");
+        System.out.println("or input 4 for division");
     }
 }
