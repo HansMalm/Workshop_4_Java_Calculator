b2e4f1328dbb801951b0765fc4511a686b1cc98e
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-02-26 13:53:06 +0100

    modify main.java file
    add simple loop menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,24 @@ import java.util.Scanner;
 public class Main {
     static void main() {
 
-        int firstNumber, secondNumber;
+        int number, firstNumber, secondNumber;
         Scanner scanner = new Scanner(System.in);
         boolean end = false;
 
         System.out.println("Welcome to the Calculator!");
-        System.out.println("input 1 for addition");
-        System.out.println("input 2 for subtraction");
-        System.out.println("input 3 for multiplication");
-        System.out.println("or input 4 for division");
+        while (true) {
+            System.out.println("Enter a number to calculate.");
+            System.out.print("input:");
+            number = scanner.nextInt();
+            System.out.println("number = " + number);
+            System.out.println("input (1) to calculate a new number,");
+            System.out.println("or input (2) to end.");
+            System.out.print("input: ");
+            number = scanner.nextInt();
+            if (number == 0) {
+                System.out.print("End.");
+                break;
+            }
+        }
     }
 }
